fd25b74d4ced22d82e5ad51184bae49474c02847
Sebastian Gómez Palacios - Ceiba Software <sebastian.gomez@ceiba.com.co>
2021-03-05 14:34:18 -0500

    esquema base de datos

--- cursos/src/main/resources/db/migration/DDL/V1.0__schema.sql
+++ cursos/src/main/resources/db/migration/DDL/V1.0__schema.sql
@@ -1,7 +1,18 @@
 create table usuario (
  id int(11) not null auto_increment,
+ cedula int(11) not null,
  nombre varchar(100) not null,
  clave varchar(45) not null,
  fecha_creacion datetime null,
  primary key (id)
 );
+
+create table inscripcion (
+ id int(11) not null auto_increment,
+ usuario int(11) not null,
+ curso varchar(100) not null,
+ valor varchar(45) not null,
+ fecha_inscripcion datetime null,
+ primary key (id),
+ FOREIGN KEY (usuario) REFERENCES usuario(id)	
+);  
